f75af78b6dd6b6319911b73adbf3248d377d5ede
Allwin P G <allwin.george19@gmail.com>
2024-11-16 02:07:15 +0530

    second commit after structuring folders and navbar building

---
+++ src/component/Title Cards/TiltleCards.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './TiltleCards.css'
+
+const TiltleCards = () => {
+  return (
+    <div className='titlecards'>TiltleCards</div>
+  )
+}
+
+export default TiltleCards
